20a7c0e9e1d3b3e9df0bc326170003947b4dc427
David Craig <david@dcraig.dev>
2022-07-26 13:39:43 +0100

    Create adblock guide and generic page component.

---
+++ Components/Page.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+interface PageComponentProps {
+    title?: string;
+    slug?: string;
+    children?: any;
+}
+
+const Page: NextPage = (props: PageComponentProps) => {
+  const { title, slug, children } = props
+  return (
+    <div>
+      <Head>
+        <title>Crypto Safety Squad | Home</title>
+        <meta name="description" content="A community project dedicated to onboarding new users safely into CryptoCurrencies." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='max-w-7xl m-auto py-4 px-2 md:py-12 md:px-4 bg-dark rounded-md'>
+        {children}
+        {/* <Discussion slug='guides/adblock' title='AdBlocking' /> */}
+      </main>
+    </div>
+  )
+}
+
+export default Page
