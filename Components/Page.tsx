e28016b5b2e09a27145916a71b491bdb9b7c20aa
David Craig <david@dcraig.dev>
2022-09-28 13:21:38 +0100

    Fix title being classed as 2 elements.

--- Components/Page.tsx
+++ Components/Page.tsx
@@ -14,7 +14,7 @@ const Page = ({ title, slug, content, description, author }: PageComponentProps)
   return (
     <div>
       <Head>
-        <title>Crypto Safety Squad | {title}</title>
+        <title>Crypto Safety Squad | {`${title}`}</title>
         <meta name="description" content={description} />
       </Head>
 
@@ -23,7 +23,7 @@ const Page = ({ title, slug, content, description, author }: PageComponentProps)
       <main className='max-w-7xl m-auto py-4 px-2 md:py-12 md:px-4 bg-dark rounded-md'>
         <div className='xl:grid grid-cols-[70%,1fr] gap-16'>
           <div>
-            <h1 className='mt-6 mb-12'>{title}</h1>
+            <h1 className='mt-6 mb-12'>{`${title}`}</h1>
             {content}
           </div>
           {author && (
