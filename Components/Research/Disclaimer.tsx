106665e7d1d378ece58ce013ac1956b97f2c492a
David Craig <david@dcraig.dev>
2022-09-30 14:10:35 +0100

    Create disclaimer component.

---
+++ Components/Research/Disclaimer.tsx
@@ -0,0 +1,3 @@
+const Disclaimer = () => <p className='warn'>Disclaimer: This is just a high level of my own basic research, it is not financial advice, you should conduct your own research before investing into any project. I accept no responsibility for your own investing decisions or any outcomes from investing in any project.</p>
+
+export default Disclaimer
