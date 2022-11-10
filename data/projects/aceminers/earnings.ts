a27e7a9c10b9e04b974523dd04ae85f697d6b94c
David Craig <david@dcraig.dev>
2022-11-10 23:19:51 +0000

    Create page for ace miners.

---
+++ data/projects/aceminers/earnings.ts
@@ -0,0 +1,11 @@
+interface earningsEntry {
+  label: string;
+  date: string;
+  amount: number;
+}
+
+const earnings: earningsEntry[] = [
+  { label: 'Oct 22', date: '2022-10-20', amount: 16 }
+]
+
+export default earnings
