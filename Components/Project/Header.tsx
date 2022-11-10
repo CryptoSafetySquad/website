5b45ec246ba00d5f566ba089bfce8a49eeb45435
David Craig <david@dcraig.dev>
2022-11-10 21:50:21 +0000

    Simplify project79 page.

--- Components/Project/Header.tsx
+++ Components/Project/Header.tsx
@@ -5,19 +5,21 @@ interface ProjectHeaderProps {
   website?: string;
   opensea?: string;
   className?: string;
+  description?: any;
 }
 
-const Header = ({ className, name, website, opensea }: ProjectHeaderProps) => {
+const Header = ({ className, name, website, opensea, description }: ProjectHeaderProps) => {
   const linkIcon = <img className='mr-2' src='/images/icons/external.svg' height='20' width='20' />
   const openseaIcon = <img className='mr-2' src='/images/icons/opensea.svg' height='22' width='22' />
 
   return (
     <div>
-      <h1 className={`flex ${className}`}>
+      <h1 className={`flex ${className} mb-2`}>
         <span className='mr-4'>{name}</span>
         {website && <ExternalLink href={`${website}`}>{linkIcon}</ExternalLink>}
         {opensea && <ExternalLink href={`${opensea}`}>{openseaIcon}</ExternalLink>}
       </h1>
+      {description}
     </div>
   )
 }
--- pages/projects/project79.tsx
+++ pages/projects/project79.tsx
@@ -13,12 +13,7 @@ import {
 } from 'chart.js';
 
 import Page from '../../Components/Page'
-import ExternalLink from '../../Components/ExternalLink'
-import TabbedContent from '../../Components/TabbedContent'
-import AuthorComponent from '../../Components/Author'
-import Disclaimer from '../../Components/Research/Disclaimer'
 import Header from '../../Components/Project/Header'
-import PrivateSniper from '../../data/authors/PrivateSniper'
 import earnings from '../../data/projects/project79/earnings'
 import tvl from '../../data/projects/project79/tvl'
 
@@ -31,52 +26,6 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-
-const doxxed = () => {
-  return (
-    <>
-      Core team fully <abbr title='Doxxed means personal data is available about team members, photo, work history, social media profiles etc.'>doxxed</abbr>: <ExternalLink href='https://www.hydrowhalesclub.com/#team' />
-    </>
-  )
-}
-
-const Research = () => {
-  const tick = '✅'
-  const warn = '⚠️'
-
-  const score = 10
-
-  const author = PrivateSniper
-  const goodPoints = [
-    { key: 'doxxed', content: doxxed() },
-    { key:'legal', content: 'Has legal team on retainer' },
-    { key:'whitepaper', content: 'Full whitepaper' },
-    { key:'wayback', content: (<>Website content is captured in <ExternalLink href='https://web.archive.org/web/20220711192706/https://www.hydrowhalesclub.com/' text='wayback machine' /></>) }
-  ]
-  
-  const redFlags = []
-  
-  const overview = {
-    project: 'Hydro Whales Mining Club',
-    slug: 'hydro-whales',
-    researcher: PrivateSniper,
-    points: { good: goodPoints.length, bad: redFlags.length },
-    score
-  }
-
-  const researchContent = (
-    <div className='guide'>
-      <p>See research for <Link href='/projects/hydro-whales'><a>Hydro Whales Mining Club</a></Link> as is ran by the same team.</p>
-    </div>
-  )
-
-  return (
-    <div>
-      <h1 className='mb-6'>Project 79 Research</h1>
-      {researchContent}
-    </div>
-  )
-}
 
 const NewsContent = () => {
   const news = [
@@ -159,28 +108,19 @@ const StatsContent = () => {
 
 const Project79: NextPage = () => {
   const title = 'Project 79'
-  const tabs = [
-    {
-      title: 'News',
-      content: <NewsContent />
-    },
-    {
-      title: 'Research',
-      content: <Research />
-    }
-  ]
 
   const content = (
     <>
-      <div className='grid gap-2 grid-cols-[4fr_1fr]'>
+      <div className='grid gap-2 grid-cols-[3fr_1fr]'>
         <div className='card'>
-          <TabbedContent content={tabs} />
+          <h2 className='text-gold'>News</h2>
+          <NewsContent />
         </div>
         <div>
           <StatsContent />
 
           <div className='card'>
-            <h2>Related Projects</h2>
+          <h2 className='text-gold'>Related Projects</h2>
             <ul>
               <li><Link href='/projects/hydro-whales'><a>Hydro Whales Mining Club</a></Link></li>
             </ul>
@@ -197,7 +137,12 @@ const Project79: NextPage = () => {
       content,
       slug: 'projects/project79',
       description: 'Project 79',
-      header: <Header className='text-gold' name={title} website='https://www.project79.com/' />
+      header: <Header
+        className='text-gold'
+        name={title}
+        website='https://www.project79.com/'
+        description={<p>A <Link href='/projects/hydro-whales'><a>HydroWhales Mining Club</a></Link> launchpad project</p>}
+      />
     }} />
   )
 }
