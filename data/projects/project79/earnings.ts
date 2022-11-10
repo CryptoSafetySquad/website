2bad6c0342c90a17979b304d6f4d3fde2b9576c8
David Craig <david@dcraig.dev>
2022-11-10 22:15:24 +0000

    Enhance typed information on p79 earnings and tvl.

--- data/projects/project79/earnings.ts
+++ data/projects/project79/earnings.ts
@@ -1,4 +1,10 @@
-const earnings = [
+interface earningsEntry {
+  label: string;
+  date: string;
+  percent: number;
+}
+
+const earnings: earningsEntry[] = [
   { label: 'Sep 22', date: '2022-09-26', percent: 6.8 },
   { label: 'Oct 22', date: '2022-10-24', percent: 6.4 }
 ]
--- data/projects/project79/tvl.ts
+++ data/projects/project79/tvl.ts
@@ -1,4 +1,10 @@
-const tvl = [
+interface tvlEntry {
+  label: string;
+  date: string;
+  tvl: number;
+}
+
+const tvl: tvlEntry[] = [
   { label: 'Sep 22', date: '2022-09-06', tvl: 2400000 },
   { label: 'Oct 22', date: '2022-10-06', tvl: 4800000 },
   { label: 'Nov 22', date: '2022-11-04', tvl: 7108109 }
