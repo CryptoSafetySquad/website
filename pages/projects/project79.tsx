f70351546665ef7b597b02111de23039ad35082f
David Craig <david@dcraig.dev>
2022-11-10 22:20:39 +0000

    Extract chart boilerplate.

--- pages/projects/project79.tsx
+++ pages/projects/project79.tsx
@@ -44,6 +44,35 @@ const NewsContent = () => {
   )
 }
 
+const P79Chart = ({ labels, data, title}: { labels: any[], data: any, title: string; }) => {
+  const chartHeight = '175px'
+  return (
+    <div style={{ height: chartHeight }}>
+      <Line
+        options={{
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+                beginAtZero: true
+            }
+          }
+        }}
+        data={{
+          labels: labels,
+          datasets: [
+            {
+              label: title,
+              data: data,
+              borderColor: '#b49b68',
+              backgroundColor: '#b49b68',
+            }
+          ],
+        }}
+      />
+    </div>
+  )
+}
+
 const StatsContent = () => {
   // Compile TVL data
   const tvlData: { labels: string[]; data: number[] } = { labels: [], data: [] }
@@ -59,61 +88,22 @@ const StatsContent = () => {
       {/* Earnings */}
       <div className='card'>
         <h2 className='text-gold'>Earnings</h2>
-        <div style={{ height: chartHeight }}>
-          <Line
-            // datasetIdKey='id'
-            options={{
-              maintainAspectRatio: false,
-              scales: {
-                y: {
-                    beginAtZero: true
-                }
-              }
-            }}
-            data={{
-              labels: earningsData.labels,
-              datasets: [
-                {
-                  label: 'Earnings',
-                  data: earningsData.data,
-                  borderColor: '#b49b68',
-                  backgroundColor: '#b49b68',
-                }
-              ],
-            }}
-          />
-        </div>
+        <P79Chart
+          labels={earningsData.labels}
+          title='Earnings'
+          data={earningsData.data}
+        />
       </div>
     
       {/* TVL */}
       <div className='card'>
         <h2 className='text-gold'>TVL</h2>
-        <div style={{ height: chartHeight }}>
-          <Line
-            // datasetIdKey='id'
-            options={{
-              maintainAspectRatio: false,
-              scales: {
-                y: {
-                    beginAtZero: true
-                }
-              }
-            }}
-            data={{
-              labels: tvlData.labels,
-              datasets: [
-                {
-                  label: 'TVL',
-                  data: tvlData.data,
-                  borderColor: '#b49b68',
-                  backgroundColor: '#b49b68',
-                }
-              ],
-            }}
-          />
-        </div>
+        <P79Chart
+          labels={tvlData.labels}
+          title='TVL'
+          data={tvlData.data}
+        />
       </div>
-      
     </>
   )
 }
