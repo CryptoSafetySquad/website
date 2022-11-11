76e21a749d59a20faf6070fcc67185b37d3200dc
David Craig <david@dcraig.dev>
2022-11-11 00:51:11 +0000

    Add twitter link to person page.

--- Components/Person.tsx
+++ Components/Person.tsx
@@ -19,6 +19,12 @@ const Person = ({ name, content, slug, person, sidebarContent }) => {
                 {person.socials.discord}
               </div>
           )}
+          {person.socials.twitter && (
+              <div className='card flex'>
+                {/* <img className='mr-2' src='/images/icons/discord.svg' width='24' height='24' alt='Discord Username:' /> */}
+                <a rel='noopener noreferrer' target='_blank' href={`https://twitter.com/${person.socials.twitter}`}>@{person.socials.twitter}</a>
+              </div>
+          )}
           {sidebarContent ? sidebarContent : null}
         </>
       )
