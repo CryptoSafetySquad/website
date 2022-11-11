b6ba7dca8ece2e22c0e29f44df726fb260dddf75
David Craig <david@dcraig.dev>
2022-11-11 00:21:41 +0000

    Add disclaimer to projects page.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -12,26 +12,30 @@ const projects = [
 const Projects: NextPage = () => {
   const title = 'Projects'
   const content = (
-    <table className='table'>
-      <thead>
-        <tr>
-          <th>Project</th>
-          <th>Type</th>
-          <th>Blockchain</th>
-        </tr>
-      </thead>
-      <tbody>
-      {
-        projects.map(project => 
-          <tr key={project.slug}>
-            <td><a href={`/projects/${project.slug}`}>{project.name}</a></td>
-            <td>{project.type}</td>
-            <td>{project.blockchain}</td>
+    <>
+      <p>The projects here are just those we have done some research into, the content herein does not constitute financial advice</p>
+      <p>Please do your own research before investing, we can take no responsibility for the performance of any investments you make.</p>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Project</th>
+            <th>Type</th>
+            <th>Blockchain</th>
           </tr>
-        )
-      }
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        {
+          projects.map(project => 
+            <tr key={project.slug}>
+              <td><a href={`/projects/${project.slug}`}>{project.name}</a></td>
+              <td>{project.type}</td>
+              <td>{project.blockchain}</td>
+            </tr>
+          )
+        }
+        </tbody>
+      </table>
+    </>
   )
 
   return (
