30464df292fe9da93687803a5c762b90bcc385c7
David Craig <david@dcraig.dev>
2022-11-12 16:50:15 +0000

    Improve spacing of mobile navbar items.

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,15 +2,15 @@ import Link from "next/link"
 
 const Navbar = () => (
   <div className='p-2 md:p-4 flex flex-col md:flex-row'>
-    <div className='xl:ml-16 mr-4 xl:mr-16'>
+    <div className='xl:ml-16 mr-4 xl:mr-16 mb-4 md:mb-0'>
       <Link href='/'><a>Crypto Safety Squad</a></Link>
     </div>
     <div className='flex flex-col md:flex-row'>
-      <Link href='/guides'><a className='mr-8'>Guides</a></Link>
-      <Link href='/projects'><a className='mr-8'>Projects</a></Link>
-      <Link href='/general-tips'><a className='mr-8'>General Tips</a></Link>
-      <Link href='/referrals'><a className='mr-8'>Referrals</a></Link>
-      <Link href='/videos'><a className='mr-8'>Video&apos;s</a></Link>
+      <Link href='/guides'><a className='mr-8 mb-2 md:mb-0'>Guides</a></Link>
+      <Link href='/projects'><a className='mr-8 mb-2 md:mb-0'>Projects</a></Link>
+      <Link href='/general-tips'><a className='mr-8 mb-2 md:mb-0'>General Tips</a></Link>
+      <Link href='/referrals'><a className='mr-8 mb-2 md:mb-0'>Referrals</a></Link>
+      <Link href='/videos'><a className='mr-8 mb-2 md:mb-0'>Video&apos;s</a></Link>
     </div>
     <div className='sm:flex md:ml-auto mr-4 xl:mr-16 hidden'>
       <a
