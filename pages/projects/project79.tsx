45b03450b4b26f52e6477096699838e3e335435a
David Craig <david@dcraig.dev>
2022-11-13 09:04:25 +0000

    Make date format consistent.

--- pages/projects/project79.tsx
+++ pages/projects/project79.tsx
@@ -33,7 +33,7 @@ function average(nums: number[]) {
 
 const NewsContent = () => {
   const news = [
-    { date: '4/11/2022', content: '3rd sacrifice period closed, TVL $7.1m' },
+    { date: '04/11/2022', content: '3rd sacrifice period closed, TVL $7.1m' },
     { date: '29/10/2022', content: (<>James Pelton AMA - <a href='https://www.youtube.com/watch?v=zpd8L2rlLgg' rel='noopener noreferrer' target='_blank'>AMA with Project 79: Making 6%/month With Actual Revenue</a></>) },
     { date: '24/10/2022', content: '2nd Sacrifice earnings of 6.4%' },
     { date: '26/09/2022', content: (<>1st Sacrifice earnings of 6.8%, plus new <a href='https://sacrifice.project79.com/' rel='noreferrer noopener' target='_blank'>sacrifice dashboard</a> launched</>) },
