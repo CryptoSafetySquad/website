11d650f000a69a71633fd9bdfbd755d6f6ea646a
David Craig <david@dcraig.dev>
2022-11-14 21:34:04 +0000

    Add banner for ocean money.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,7 +5,7 @@ import Page from '../Components/Page'
 const projects = [
   {name: 'Hydro Whales Mining Club', slug: 'hydro-whales', type: 'NFT: Utility, Passive Income', blockchain: 'Ethereum', banner: 'https://i.seadn.io/gae/rgToigCSJptmZH_aXssvgDJ7JLtSL0QqjfncgOgoNwjaZxOeS7qD5p6CB1Pdp1P3cUlouqnfQUCe2_ux_lljAc1GcVSLhBueuuelSnY?auto=format&w=1920'},
   {name: 'Project 79', slug: 'project79', type: 'Passive Income', blockchain: 'Ethereum'},
-  {name: 'Ocean Money', slug: 'ocean-money', type: 'Banking Services, Crypto Services', blockchain: 'N/A'},
+  {name: 'Ocean Money', slug: 'ocean-money', type: 'Banking Services, Crypto Services', blockchain: 'N/A', banner: 'https://www.ocean.money/_next/image?url=%2Fimages%2Fblog-1.png&w=1920&q=75'},
   {name: 'OMNIBirds', slug: 'omnibirds', type: 'NFT: Utility, Passive Income', blockchain: 'Ethereum', banner: 'https://i.seadn.io/gcs/files/e66305d2587c0ea7e29d7c1c1cd3cae5.png?auto=format&w=1920'},
   {name: 'Ace Miners NFT', slug: 'aceminers', type: 'NFT: Utility, Passive Income', blockchain: 'Ethereum', banner: 'https://i.seadn.io/gcs/files/f59537bf481f49ad3d97a8de0ac164d0.png?auto=format&w=1920'},
 ]
