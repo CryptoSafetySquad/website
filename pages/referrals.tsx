7ce7cc85d2856472704b25f2dc9ee62207a23fec
David Craig <david@dcraig.dev>
2022-11-18 09:48:24 +0000

    Update referrals page.

--- pages/referrals.tsx
+++ pages/referrals.tsx
@@ -1,50 +1,30 @@
 import type { NextPage } from 'next'
 import Page from '../Components/Page'
 
-const exchanges = [
-  { name: 'KuCoin', href: 'https://www.kucoin.com/r/rf/rJQQK1S' },
-  { name: 'Binance', href: 'https://www.binance.me/en/activity/referral-entry/CPA?fromActivityPage=true&ref=CPA_00Z6XZLBQ0' }
-]
-const trading = [
-  { name: 'Pionex', href: 'https://www.pionex.com/en-US/sign/ref/idNmc3r4' }
-]
-
 const Referrals: NextPage = () => {
   const title = 'Referrals'
   const content = (
     <>
       <p>For some referrals we may make some bonus commissions, or you may make some, or both, using our links is a way to support our authors for providing this free resource.</p>
 
-      <h2 className='text-secondary'>Exchanges</h2>
-      <ul>
-        {
-          exchanges.map((e) => (
-            <li key={e.name}>
-              <a href={e.href} target='_blank' rel='noopener noreferrer'>
-                {e.name}
-              </a>
-            </li>
-          ))
-        }
-      </ul>
+      <h2 className='text-secondary mb-2'>Exchanges</h2>
+      <div className='grid grid-cols-4 gap-2'>
+        <a className='mb-2' href='https://www.kucoin.com/r/rf/rJQQK1S' target='_blank' rel='noopener noreferrer'>
+          <img alt='KuCoin' src='https://assets.staticimg.com/cms/media/1lB3PkckFDyfxz6VudCEACBeRRBi6sQQ7DDjz0yWM.svg' />
+        </a>
+      </div>
 
-      <h2 className='text-secondary'>Trading Platforms</h2>
-      <ul>
-        {
-          trading.map((t) => (
-            <li key={t.name}>
-              <a href={t.href} target='_blank' rel='noopener noreferrer'>
-                {t.name}
-              </a>
-            </li>
-          ))
-        }
-      </ul>
+      <h2 className='text-secondary mt-4 mb-2'>Trading Platforms</h2>
+      <div className='grid grid-cols-4 gap-2'>
+        <a className='mb-2' href='https://www.pionex.com/en-US/sign/ref/idNmc3r4' target='_blank' rel='noopener noreferrer'>
+          <img alt='Pionex' src='https://www.pionex.com/static/media/icon_nav_logo.c0918ec1.svg' />
+        </a>
+      </div>
 
-      <h2 className='text-secondary'>Author donations</h2>
+      <h2 className='text-secondary mt-4 mb-2'>Author donations</h2>
       <p>If you would like to donate any funds to our authors they would be very appreciative.</p>
-      <div className='grid grid-cols-3'>
-        <div>
+      <div className='grid grid-cols-3 mt-4'>
+        <div className='card'>
           <h2>PrivateSniper</h2>
           <p>Btc: <span className='text-secondary'>bc1q9hzu2hwe5g0c7mks4jd0nzm65qle62njdu7zcg</span></p>
           <p>Eth: <span className='text-secondary'>0xF311fFe5b49fdE6BdED1342694EDdD7C9003a922</span></p>
