0509b876884cda18844df6abfbb263f41e06d6a9
David Craig <david@dcraig.dev>
2022-11-19 14:01:44 +0000

    Refactor.

--- pages/ecosystems/ethereum.tsx
+++ pages/ecosystems/ethereum.tsx
@@ -6,40 +6,40 @@ import ExternalLink from '../../Components/ExternalLink'
 const Ethereum: NextPage = () => {
   const title = 'Ethereum'
   const content = (
-    <>
-      <div className='grid grid-cols-3 gap-2'>
-        <div className='card'>
-          <h2 className='mb-2'>Block Explorer</h2>
-          <p><ExternalLink href='https://etherscan.io' text='Etherscan' /></p>
-        </div>
+    <div className='grid grid-cols-3 gap-2'>
+      <div className='card'>
+        <h2 className='mb-2'>Block Explorer</h2>
+        <p><ExternalLink href='https://etherscan.io' text='Etherscan' /></p>
+      </div>
 
-        <div className='card'>
-          <h2 className='mb-2'>Exchanges (Centralised)</h2>
-          <p><ExternalLink href='https://www.kucoin.com/r/rf/rJQQK1S' text='KuCoin' /></p>
-        </div>
+      <div className='card'>
+        <h2 className='mb-2'>Exchanges (Centralised)</h2>
+        <p><ExternalLink href='https://www.kucoin.com/r/rf/rJQQK1S' text='KuCoin' /></p>
+      </div>
 
-        <div className='card'>
-          <h2 className='mb-2'>DEXs (Token Swap)</h2>
-          <div className='grid grid-cols-2 gap-2'>
-            <ExternalLink href='https://app.uniswap.org/' text='UniSwap' />
-            <ExternalLink href='https://pancakeswap.finance/' text='PancakeSwap' />
-          </div>
-        </div>
-
-        <div className='card'>
-          <h2 className='mb-2'>NFT Marketplaces</h2>
-          <div className='grid grid-cols-2 gap-2'>
-            <ExternalLink href='https://opensea.io/' text='OpenSea' />
-            <ExternalLink href='https://x2y2.io/' text='X2Y2' />
-          </div>
-        </div>
-
-        <div className='card'>
-          <h2 className='mb-2'>DeFI (Decentralised Finance)</h2>
-          <p><ExternalLink href='https://app.beefy.com/' text='Beefy' /></p>
+      <div className='card'>
+        <h2 className='mb-2'>DEXs (Token Swap)</h2>
+        <div className='grid grid-cols-2 gap-2'>
+          <ExternalLink href='https://app.uniswap.org/' text='UniSwap' />
+          <ExternalLink href='https://pancakeswap.finance/' text='PancakeSwap' />
         </div>
       </div>
-    </>
+
+      <div className='card'>
+        <h2 className='mb-2'>NFT Marketplaces</h2>
+        <div className='grid grid-cols-2 gap-2'>
+          <ExternalLink href='https://opensea.io/' text='OpenSea' />
+          <ExternalLink href='https://x2y2.io/' text='X2Y2' />
+        </div>
+      </div>
+
+      <div className='card'>
+        <h2 className='mb-2'>DeFI (Decentralised Finance)</h2>
+        <div className='grid grid-cols-2 gap-2'>
+          <ExternalLink href='https://app.beefy.com/' text='Beefy' />
+        </div>
+      </div>
+    </div>
   )
 
   return (
