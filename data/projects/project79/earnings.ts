196cbe8a21c1b7ba70ca27148475307345d7b507
David Craig <david@dcraig.dev>
2022-11-25 16:48:43 +0000

    P79 Nov earnings 6.5%

--- data/projects/project79/earnings.ts
+++ data/projects/project79/earnings.ts
@@ -6,7 +6,8 @@ interface earningsEntry {
 
 const earnings: earningsEntry[] = [
   { label: 'Sep 22', date: '2022-09-26', percent: 6.8 },
-  { label: 'Oct 22', date: '2022-10-24', percent: 6.4 }
+  { label: 'Oct 22', date: '2022-10-24', percent: 6.4 },
+  { label: 'Nov 22', date: '2022-10-24', percent: 6.5 }
 ]
 
 export default earnings
