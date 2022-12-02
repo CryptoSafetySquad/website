e54fe81328a0db96e736876c3a9b55c4c411a615
David Craig <david@dcraig.dev>
2022-12-02 13:59:33 +0000

    Convert news to tsx to allow for react elements (such as links).

---
+++ data/projects/project79/news.tsx
@@ -0,0 +1,12 @@
+const news = [
+  { date: '25/11/2022', content: 'Next sacrifice period will also support BSC' },
+  { date: '25/11/2022', content: '3rd Sacrifice earnings: 6.5%' },
+  { date: '04/11/2022', content: '3rd sacrifice period closed, TVL $7.1m' },
+  { date: '29/10/2022', content: (<>James Pelton AMA - <a href='https://www.youtube.com/watch?v=zpd8L2rlLgg' rel='noopener noreferrer' target='_blank'>AMA with Project 79: Making 6%/month With Actual Revenue</a></>) },
+  { date: '24/10/2022', content: '2nd Sacrifice earnings of 6.4%' },
+  { date: '26/09/2022', content: (<>1st Sacrifice earnings of 6.8%, plus new <a href='https://sacrifice.project79.com/' rel='noreferrer noopener' target='_blank'>sacrifice dashboard</a> launched</>) },
+  { date: '13/09/2022', content: 'P79 discord launched' },
+  { date: '02/08/2022', content: 'Project 79 announced!' },
+]
+
+export default news
--- pages/projects/project79.tsx
+++ pages/projects/project79.tsx
@@ -16,6 +16,7 @@ import Page from '../../Components/Page'
 import Header from '../../Components/Project/Header'
 import earnings from '../../data/projects/project79/earnings'
 import tvl from '../../data/projects/project79/tvl'
+import news from '../../data/projects/project79/news'
 
 ChartJS.register(
   CategoryScale,
@@ -32,16 +33,6 @@ function average(nums: number[]) {
 }
 
 const NewsContent = () => {
-  const news = [
-    { date: '25/11/2022', content: 'Next sacrifice period will also support BSC' },
-    { date: '25/11/2022', content: '3rd Sacrifice earnings: 6.5%' },
-    { date: '04/11/2022', content: '3rd sacrifice period closed, TVL $7.1m' },
-    { date: '29/10/2022', content: (<>James Pelton AMA - <a href='https://www.youtube.com/watch?v=zpd8L2rlLgg' rel='noopener noreferrer' target='_blank'>AMA with Project 79: Making 6%/month With Actual Revenue</a></>) },
-    { date: '24/10/2022', content: '2nd Sacrifice earnings of 6.4%' },
-    { date: '26/09/2022', content: (<>1st Sacrifice earnings of 6.8%, plus new <a href='https://sacrifice.project79.com/' rel='noreferrer noopener' target='_blank'>sacrifice dashboard</a> launched</>) },
-    { date: '13/09/2022', content: 'P79 discord launched' },
-    { date: '02/08/2022', content: 'Project 79 announced!' },
-  ]
   return (
     <>
       {
