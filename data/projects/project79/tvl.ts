b2bbe7b3ef302c56bc4a5792fd0489ef8c03549f
David Craig <david@dcraig.dev>
2022-12-05 16:42:23 +0000

    Add P79 nov/dec tvl.

--- data/projects/project79/tvl.ts
+++ data/projects/project79/tvl.ts
@@ -7,7 +7,8 @@ interface tvlEntry {
 const tvl: tvlEntry[] = [
   { label: 'Sep 22', date: '2022-09-06', tvl: 2400000 },
   { label: 'Oct 22', date: '2022-10-06', tvl: 4800000 },
-  { label: 'Nov 22', date: '2022-11-04', tvl: 7108109 }
+  { label: 'Nov 22', date: '2022-11-04', tvl: 7108109 },
+  { label: 'Dec 22', date: '2022-12-05', tvl: 8611727 }
 ]
 
 export default tvl
