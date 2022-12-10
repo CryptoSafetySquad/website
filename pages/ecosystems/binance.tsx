43e2378f6a6e506b06b67e9804438aee0514d748
David Craig <david@dcraig.dev>
2022-12-10 11:32:24 +0000

    Move drip to dead projects/protocols, massive negative price action.

--- pages/ecosystems/binance.tsx
+++ pages/ecosystems/binance.tsx
@@ -42,9 +42,15 @@ const Binance: NextPage = () => {
         </div>
 
         <div className='card'>
-          <h2 className='mb-2'>Protocols</h2>
+          <h2 className='mb-2'>Projects / Protocols</h2>
           <div className='grid grid-cols-2 gap-2'>
             <ExternalLink href='https://emp.money/' text='EMP Money' />
+          </div>
+        </div>
+
+        <div className='card'>
+          <h2 className='mb-2'>&quot;Dead&quot; Projects / Protocols</h2>
+          <div className='grid grid-cols-2 gap-2'>
             <ExternalLink href='https://drip.community/' text='DRIP' />
           </div>
         </div>
