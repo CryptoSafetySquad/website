01d175974c10f66e6070c29627585a7d7cc655ea
David Craig <david@dcraig.dev>
2022-12-11 11:21:34 +0000

    Add gate.io referral.

--- pages/referrals.tsx
+++ pages/referrals.tsx
@@ -10,12 +10,14 @@ const Referrals: NextPage = () => {
       <h2 className='text-secondary mb-2'>Exchanges</h2>
       <div className='grid grid-cols-4 gap-2'>
         <a className='mb-2' href='https://www.kucoin.com/r/rf/rJQQK1S' target='_blank' rel='noopener noreferrer'>
-          <img alt='KuCoin' src='https://assets.staticimg.com/cms/media/1lB3PkckFDyfxz6VudCEACBeRRBi6sQQ7DDjz0yWM.svg' />
+          <picture>
+            <img alt='KuCoin' src='https://assets.staticimg.com/cms/media/1lB3PkckFDyfxz6VudCEACBeRRBi6sQQ7DDjz0yWM.svg' />
+          </picture>
         </a>
         <a className='mb-2 flex' href='https://coinbase.com/join/craig_hcc?src=referral-link' target='_blank' rel='noopener noreferrer'>
-          <svg xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Coinbase logo" viewBox="0 0 48 48" width="32" height="32"><title>Coinbase logo</title><path d="M24,36c-6.63,0-12-5.37-12-12s5.37-12,12-12c5.94,0,10.87,4.33,11.82,10h12.09C46.89,9.68,36.58,0,24,0 C10.75,0,0,10.75,0,24s10.75,24,24,24c12.58,0,22.89-9.68,23.91-22H35.82C34.87,31.67,29.94,36,24,36z" fill="#FFFFFF"></path></svg>
-          <p className='ml-4'>Coinbase</p>
+          Coinbase
         </a>
+        <a href='https://www.gate.io/signup/11213076' target='_blank' rel='noopener noreferrer'>Gate.io</a>
       </div>
 
       <h2 className='text-secondary mt-4 mb-2'>Trading Platforms</h2>
