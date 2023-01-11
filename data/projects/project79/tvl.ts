c2b34df070e26cbdc6a75aea0ffd919272af6bfd
David Craig <david@dcraig.dev>
2023-01-11 20:24:34 +0000

    Add dec22-jan23 TVL

--- data/projects/project79/tvl.ts
+++ data/projects/project79/tvl.ts
@@ -8,7 +8,8 @@ const tvl: tvlEntry[] = [
   { label: 'Sep 22', date: '2022-09-06', tvl: 2400000 },
   { label: 'Oct 22', date: '2022-10-06', tvl: 4800000 },
   { label: 'Nov 22', date: '2022-11-04', tvl: 7108109 },
-  { label: 'Dec 22', date: '2022-12-05', tvl: 8611727 }
+  { label: 'Dec 22', date: '2022-12-05', tvl: 8611727 },
+  { label: 'Jan 23', date: '2023-01-11', tvl: 12601022 }
 ]
 
 export default tvl
