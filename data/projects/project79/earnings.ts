be033a7b5a0ae61e26f269d3f81bdc6993f3209a
David Craig <david@dcraig.dev>
2023-01-29 20:50:45 +0000

    Rename interface for clarity that it is different to other earnings.

--- data/projects/project79/earnings.ts
+++ data/projects/project79/earnings.ts
@@ -1,10 +1,10 @@
-interface earningsEntry {
+interface p79EarningsEntry {
   label: string;
   date: string;
   percent: number;
 }
 
-const earnings: earningsEntry[] = [
+const earnings: p79EarningsEntry[] = [
   { label: 'Sep 22', date: '2022-09-26', percent: 6.8 },
   { label: 'Oct 22', date: '2022-10-24', percent: 6.4 },
   { label: 'Nov 22', date: '2022-11-26', percent: 6.5 },
