66b9a27f1f101e9efae67bd07cc63d08bb85725c
David Craig <david@dcraig.dev>
2023-03-04 11:44:11 +0000

    Create page for Canto

---
+++ pages/ecosystems/canto.tsx
@@ -0,0 +1,55 @@
+import type { NextPage } from 'next'
+
+import Page from '../../Components/Page'
+import ExternalLink from '../../Components/ExternalLink'
+
+const Canto: NextPage = () => {
+  const title = 'Canto'
+  const content = (
+    <div className='grid md:grid-cols-3 gap-2'>
+      <div className='card'>
+        <h2 className='mb-2'>Block Explorer</h2>
+      </div>
+
+      <div className='card'>
+        <h2 className='mb-2'>Exchanges (Centralised)</h2>
+        <div className='grid grid-cols-2 gap-2'>
+          {/* <ExternalLink href='https://www.kucoin.com/r/rf/rJQQK1S' text='KuCoin' />
+          <ExternalLink href='https://www.gate.io/signup/11213076' text='Gate.io' /> */}
+        </div>
+      </div>
+
+      <div className='card'>
+        <h2 className='mb-2'>DEXs (Token Swap)</h2>
+        <div className='grid grid-cols-2 gap-2'>
+        </div>
+      </div>
+
+      <div className='card'>
+        <h2 className='mb-2'>NFT Marketplaces</h2>
+        <div className='grid grid-cols-2 gap-2'>
+          {/* <ExternalLink href='https://www.alphashares.io/' text='Alpha Shares' /> */}
+        </div>
+      </div>
+
+      <div className='card'>
+        <h2 className='mb-2'>DeFI (Decentralised Finance)</h2>
+        <div className='grid grid-cols-2 gap-2'>
+            {/* <ExternalLink href='https://yieldwolf.finance/' text='YieldWolf' /> */}
+          <ExternalLink href='https://dexvaults.com/' text='dexVaults+' />
+        </div>
+      </div>
+    </div>
+  )
+
+  return (
+    <Page {...{
+      title,
+      content,
+      slug: 'ecosystems/canto',
+      description: 'Canto'
+    }} />
+  )
+}
+
+export default Canto
