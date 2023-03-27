625a480641585b59d18c725654cf19dd378e4abf
David Craig <david@dcraig.dev>
2023-03-28 00:03:02 +0100

    Remove unused imports.

--- pages/ecosystems/bitcoin.tsx
+++ pages/ecosystems/bitcoin.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from 'next'
 import type { EcosystemLink } from '../../types/EcosystemLink'
 
-import Page from '../../Components/Page'
-import ExternalLink from '../../Components/ExternalLink'
-import ModeSwitchingIcon from '../../Components/ModeSwitchingIcon'
 import EcosystemPage from '../../Components/EcosystemPage'
 import exchanges from '../../data/exchanges'
 
