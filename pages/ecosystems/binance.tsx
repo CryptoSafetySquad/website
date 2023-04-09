46d98892a62cd7ac2784111bc0d464ecdbd429f8
David Craig <david@dcraig.dev>
2023-04-09 15:25:03 +0100

    Fix BscScan link.

--- pages/ecosystems/binance.tsx
+++ pages/ecosystems/binance.tsx
@@ -2,28 +2,22 @@ import type { NextPage } from 'next'
 import type { EcosystemLink } from '../../types/EcosystemLink'
 
 import EcosystemPage from '../../Components/EcosystemPage'
+
 import exchanges from '../../data/exchanges'
-
-
+import marketplaces from '../../data/marketplaces'
+import defi from '../../data/defi'
 
 const data: { [key: string]: EcosystemLink[] } = {
   explorers: [
-    { name: 'BscScan', link: '', },
+    { name: 'BscScan', link: 'https://bscscan.com/', },
   ],
   centralisedExchanges: [],
   decentralisedExchanges: [],
-  defi: [
-    { name: 'Beefy', link: 'https://app.beefy.com/' },
-    { name: 'Magik', link: 'https://magik.farm/#/bsc' },
-    { name: 'Autofarm', link: 'https://autofarm.network/' },
-    { name: 'YieldWolf', link: 'https://yieldwolf.finance/' },
-    { name: 'dexVaults+', link: 'https://dexvaults.com/' },
-  ],
+  defi: [],
   protocols: [
     { name: 'EMP Money', link: 'https://emp.money/' },
   ],
   nftMarketplaces: [
-    { name: 'Alpha Shares', link: 'https://www.alphashares.io/' },
   ],
   riskyProjects: [
     { name: 'DRIP', link: 'https://drip.community/' },
@@ -34,9 +28,15 @@ data.centralisedExchanges.push(exchanges.kucoin)
 data.centralisedExchanges.push(exchanges.gateio)
 data.decentralisedExchanges.push(exchanges.uniswap)
 data.decentralisedExchanges.push(exchanges.pancakeswap)
+data.decentralisedExchanges.push(exchanges.apeswap)
 data.decentralisedExchanges.push({ name: 'Aequinox', link: 'https://www.aequinox.exchange/#/' })
-data.decentralisedExchanges.push({ name: 'ApeSwap', link: 'https://apeswap.finance/' })
 data.decentralisedExchanges.push({ name: 'Thena', link: 'https://thena.fi/' })
+data.nftMarketplaces.push(marketplaces.alphaShares)
+data.defi.push(defi.autofarm)
+data.defi.push(defi.beefy)
+data.defi.push(defi.magik)
+data.defi.push(defi.yieldwolf)
+data.defi.push(defi.dexVaults)
 
 const Binance: NextPage = () => {
   return <EcosystemPage name='Binance' slug='binance' data={data} />
