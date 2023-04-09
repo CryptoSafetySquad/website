cb90a8a70f0ea60cf3d38fb553a6bf4154d1fa59
David Craig <david@dcraig.dev>
2023-04-09 15:25:18 +0100

    Add defi data.

---
+++ data/defi.ts
@@ -0,0 +1,9 @@
+const defi = {
+  beefy: { name: 'Beefy', link: 'https://app.beefy.com/' },
+  magik: { name: 'Magik', link: 'https://magik.farm/#/bsc' },
+  autofarm: { name: 'Autofarm', link: 'https://autofarm.network/' },
+  yieldwolf: { name: 'YieldWolf', link: 'https://yieldwolf.finance/' },
+  dexVaults: { name: 'dexVaults+', link: 'https://dexvaults.com/' },
+}
+
+export default defi
