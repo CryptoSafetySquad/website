18a8a52c9c3abe92a4415c2db14fb6d5b70d259b
David Craig <david@dcraig.dev>
2023-04-09 15:32:59 +0100

    Add wallets info.

---
+++ data/wallets.ts
@@ -0,0 +1,7 @@
+const wallets = {
+  trustwallet: { name: 'TrustWallet', link: 'https://trustwallet.com/' },
+  metamask: { name: 'MetaMask', link: 'https://metamask.io/' },
+  rabby: { name: 'Rabby', link: 'https://rabby.io/' },
+}
+
+export default wallets
