56a251d3c1fcf8b02c262a72dc8f7213cc724c15
David Craig <david@dcraig.dev>
2023-04-09 15:25:32 +0100

    Add marketplaces data

---
+++ data/marketplaces.ts
@@ -0,0 +1,6 @@
+const marketplaces = {
+  alphaShares: { name: 'Alpha Shares', link: 'https://www.alphashares.io/' },
+  opensea: { name: 'OpenSea', link: 'https://opensea.io/' },
+}
+
+export default marketplaces
