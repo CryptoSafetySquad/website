0ae5a95fa046f32902e10c798eaf7c8542599c00
David Craig <david@dcraig.dev>
2023-04-09 15:33:06 +0100

    Add more marketplaces

--- data/marketplaces.ts
+++ data/marketplaces.ts
@@ -1,6 +1,9 @@
 const marketplaces = {
   alphaShares: { name: 'Alpha Shares', link: 'https://www.alphashares.io/' },
   opensea: { name: 'OpenSea', link: 'https://opensea.io/' },
+  x2y2: { name: 'x2y2', link: 'https://x2y2.io/' },
+  blur: { name: 'Blur', link: 'https://blur.io/' },
+  magiceden: { name: 'Magic Eden', link: 'https://magiceden.io/' },
 }
 
 export default marketplaces
