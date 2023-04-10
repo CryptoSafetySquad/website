14fd4becb613fd32154cc5d0d3df953eb86c7384
David Craig <david@dcraig.dev>
2023-04-10 22:35:03 +0100

    Add APR hwmc earnings.

--- data/projects/hydrowhales/earnings.ts
+++ data/projects/hydrowhales/earnings.ts
@@ -3,6 +3,7 @@ import type { EarningsEntry } from "../../../types/EarningsEntry"
 const earnings: EarningsEntry[] = [
   { label: '1 Feb 23', date: '2023-02-01', ethereum: { usdc: 10, wbtc: 10 }, amount: 20.00, notes: '$10 USDC, 0.000435 wBTC' },
   { label: '1 Mar 23', date: '2023-03-01', ethereum: { usdc: 13, wbtc: 10 }, amount: 23.00, notes: '$13 USDC, 0.000424 wBTC' },
+  { label: '1 Apr 23', date: '2023-04-01', polygon: { usdc: 0.01, wbtc: 10 }, amount: 23.00, notes: '$0.01 USDC, 0.000346 wBTC' },
   // { label: '1 Apr 23', date: '2023-04-01', ethereum: { usdc: 13, wbtc: 10 }, amount: 23.00, notes: '$13 USDC, 0.000424 wBTC' },
 ]
 
