823a8a53514f67c8cc31edcf396989292c977af9
David Craig <david@dcraig.dev>
2023-04-10 12:58:32 +0100

    refactor optimism

--- pages/ecosystems/optimism.tsx
+++ pages/ecosystems/optimism.tsx
@@ -1,53 +1,46 @@
 import type { NextPage } from 'next'
+import type { EcosystemLink } from '../../types/EcosystemLink'
 
 import Page from '../../Components/Page'
 import ExternalLink from '../../Components/ExternalLink'
+import EcosystemPage from '../../Components/EcosystemPage'
+
+import exchanges from '../../data/exchanges'
+import wallets from '../../data/wallets'
+import defi from '../../data/defi'
+
+const data: { [key: string]: EcosystemLink[] } = {
+  explorers: [
+    { link: 'https://optimistic.etherscan.io/', name: 'Optimism Explorer (Etherscan)' }
+  ],
+  centralisedExchanges: [
+    exchanges.kucoin,
+    exchanges.gateio,
+  ],
+  decentralisedExchanges: [],
+  defi: [
+    defi.yieldwolf,
+  ],
+  protocols: [],
+  nftMarketplaces: [],
+  riskyProjects: [],
+  wallets: [
+  ]
+}
+
+const metadata = {
+  token: {
+    name: 'OP',
+    cmc: 'https://coinmarketcap.com/currencies/optimism-ethereum/'
+  }
+}
 
 const Optimism: NextPage = () => {
-  const title = 'Optimism'
-  const content = (
-    <div className='grid md:grid-cols-3 gap-2'>
-      <div className='card'>
-        <h2 className='mb-2'>Block Explorer</h2>
-      </div>
-
-      <div className='card'>
-        <h2 className='mb-2'>Exchanges (Centralised)</h2>
-        <div className='grid grid-cols-2 gap-2'>
-          <ExternalLink href='https://www.kucoin.com/r/rf/rJQQK1S' text='KuCoin' />
-          <ExternalLink href='https://www.gate.io/signup/11213076' text='Gate.io' />
-        </div>
-      </div>
-
-      <div className='card'>
-        <h2 className='mb-2'>DEXs (Token Swap)</h2>
-        <div className='grid grid-cols-2 gap-2'>
-        </div>
-      </div>
-
-      <div className='card'>
-        <h2 className='mb-2'>NFT Marketplaces</h2>
-        <div className='grid grid-cols-2 gap-2'>
-        </div>
-      </div>
-
-      <div className='card'>
-        <h2 className='mb-2'>DeFI (Decentralised Finance)</h2>
-        <div className='grid grid-cols-2 gap-2'>
-            <ExternalLink href='https://yieldwolf.finance/' text='YieldWolf' />
-        </div>
-      </div>
-    </div>
-  )
-
-  return (
-    <Page {...{
-      title,
-      content,
-      slug: 'ecosystems/optimism',
-      description: 'Optimism'
-    }} />
-  )
+  return <EcosystemPage
+    name='Optimism'
+    metadata={metadata}
+    data={data}
+  />
 }
 
 export default Optimism
