8c6ae45553c9f42a97a5675751656df56a4b446c
David Craig <david@dcraig.dev>
2023-04-17 16:47:26 +0100

    Add healthy class.

--- data/projects/project79/news.tsx
+++ data/projects/project79/news.tsx
@@ -6,7 +6,7 @@ const news = [
   { date: '28/01/2023', content: (<>Twitter spaces AMA with Phil (Gold Partner) - <a href='https://twitter.com/i/spaces/1PlKQpkWaAzxE' rel='noopener noreferrer' target='_blank'>twitter space</a></>) },
   { date: '26/01/2023', content: (<>Clarification: If transition from <span className='warn'>sacrifice</span> to new model no earnings will be earned for February, first payout from tokens will be starting March</>) },
   { date: '29/12/2022', content: (<>P79 evolves, twitter space, overview and faq - <a href='https://twitter.com/i/spaces/1DXxyvZXgDLKM' rel='noopener noreferrer' target='_blank'>twitter space</a> - <a href='https://assets.website-files.com/62f2215d13f471bdeeb9ff6e/63accb82ce1c7d69edfb7ac1_Project%2079-Evolution.pdf' rel='noopener noreferrer' target='_blank'>new litepaper</a>, 2 tokens 1 for p79 ownership, 1 for gold-pegged token.</>) },
-  { date: '28/12/2022', content: 'Dec 2022 Earnings: 6.7%' },
+  { date: '28/12/2022', content: (<>Dec 2022 Earnings: <span className="healthy">6.7%</span></>) },
   { date: '24/12/2022', content: 'The sacrifice will open on 29 December 2022 12:00 ❄️ The sacrifice will close on 6 January 2023 12:00 ☃️' },
   { date: '25/11/2022', content: (<>Next <span className='warn'>sacrifice</span> period will also support BSC</>) },
   { date: '25/11/2022', content: (<>3rd <span className='warn'>sacrifice</span> earnings: <span className='healthy'>6.5%</span></>) },
