2c82645e2dfbd2fa5055c2cc66e100989eeea2db
David Craig <david@dcraig.dev>
2023-04-25 19:31:19 +0100

    Add rugpull status.

--- data/projects/yieldrobot/yieldrobot.tsx
+++ data/projects/yieldrobot/yieldrobot.tsx
@@ -1,11 +1,13 @@
 import type { Project } from "../../../types/project"
 import Blockchains from "../../../enums/blockchains"
 import news from "./news"
+import ProjectStatus from "../../../enums/ProjectStatus"
 
 export const YieldRobot: Project = {
   name: 'YieldRobot',
   slug: 'yieldrobot',
   type: 'Passive Income',
+  status: ProjectStatus.RugPulled,
   blockchain: Blockchains.Binance,
   contracts: [
     { name: 'YieldRobot', explorerUrl: 'https://bscscan.com/address/0xb8CDa6AE6D005d56205B29797ADD341c85e59608', contract: '0xb8CDa6AE6D005d56205B29797ADD341c85e59608', short: '0xb8...9608', blockchain: 'BSC' }
