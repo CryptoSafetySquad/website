d433f00b9fb526b031fc6905fb2faf5ea016a519
David Craig <david@dcraig.dev>
2023-04-26 16:44:00 +0100

    Add OM twitter space.

--- data/projects/oceanmoney/news.tsx
+++ data/projects/oceanmoney/news.tsx
@@ -1,5 +1,8 @@
+import ExternalLink from "../../../Components/ExternalLink"
+
 const news = [
-  { date: '02/10/2022', dateISO: '2022-10-02', content: 'Ocean Money announced on hydro whales discord' }
+  { date: '26/04/2023', dateISO: '2023-04-26', content: (<><ExternalLink text='Twitter Space' href='https://twitter.com/OceanMoney_/status/1651239267505430529' /></>) },
+  { date: '02/10/2022', dateISO: '2022-10-02', content: 'Ocean Money announced on hydro whales discord' },
 ]
 
 export default news
