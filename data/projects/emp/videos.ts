0fbb498f261acaa1f84b26cedb337257fda5a315
David Craig <david@dcraig.dev>
2023-05-02 00:43:03 +0100

    Add EMP Fusion Q&A

--- data/projects/emp/videos.ts
+++ data/projects/emp/videos.ts
@@ -3,6 +3,12 @@ import type { YouTubeVideo } from "../../../types/youtubeVideo"
 
 export const officialVideos: YouTubeVideo[] = [
   {
+    date: '01/05/2023',
+    dateISO: '2023-05-01',
+    title: 'EMP Money | Fusion : All Your Questions Answered & Updates',
+    youtubeSlug: 'AjO9SsqO_-0'
+  },
+  {
     date: '28/04/2023',
     dateISO: '2023-04-28',
     title: 'EMP Money | Fusion Whitepaper Revealed!!',
