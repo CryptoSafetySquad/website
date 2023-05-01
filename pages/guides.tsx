1a21f6d144ac266aa2146ae5c16a2b0931123e92
David Craig <david@dcraig.dev>
2023-05-01 16:09:30 +0100

    Update guides page.

--- pages/guides.tsx
+++ pages/guides.tsx
@@ -4,8 +4,8 @@ import Page from '../Components/Page'
 const guides = [
   {
     name: 'Security', guides: [
-      {name: 'Ad-blocking with uBlock Origin and Installing CryptoSafetySquad AdBlock filters', slug: 'adblock'},
-      {name: 'Discord: Disable DMs from server members.', slug: 'discorddm'},
+      {type: 'article', name: 'Ad-blocking with uBlock Origin and Installing CryptoSafetySquad AdBlock filters', slug: 'adblock'},
+      {type: 'article', name: 'Discord: Disable DMs from server members.', slug: 'discorddm'},
     ]
   }
 ]
@@ -20,9 +20,18 @@ const Guides: NextPage = () => {
             <h2 className='card__header'>{section.name}</h2>
             <div className='card__content gap-4 grid grid-cols-2 md:grid-cols-3'>
               {section.guides.map(guide =>
-                <div className='card p-4' key={guide.slug}>
-                  <a className='text-lg' href={`/guides/${guide.slug}`}>{guide.name}</a>
-                </div>
+                <>
+                  {/* {guide.type === 'video' && (
+                    <div className='card p-4' key={guide.ytVideo}>
+                      <a className='text-lg' href={`/guides/${guide.slug}`}>{guide.name}</a>
+                    </div>
+                  )} */}
+                  {guide.type === 'article' && (
+                    <div className='card p-4' key={guide.slug}>
+                      <a className='text-lg' href={`/guides/${guide.slug}`}>{guide.name}</a>
+                    </div>
+                  )}
+                </>
               )}
             </div>
           </div> 
