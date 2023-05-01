d702e7cda24517eeda8fd0718f3edb82d7273101
David Craig <david@dcraig.dev>
2023-05-01 21:31:14 +0100

    Add JP burlcore AMA 1may

--- data/projects/project79/videos.ts
+++ data/projects/project79/videos.ts
@@ -5,6 +5,12 @@ export const officialVideos: YouTubeVideo[] = [
 
 export const communityVideos: YouTubeVideo[] = [
   {
+    date: '01/05/2023',
+    dateISO: '2023-05-01',
+    title: 'Burlcore Mining: Mining Gold and Other Precious Metals',
+    youtubeSlug: 'tLilUxWciAU'
+  },
+  {
     date: '23/04/2023',
     dateISO: '2023-04-23',
     title: 'Project 79 Account Creation & KYC (step by step guide)',
