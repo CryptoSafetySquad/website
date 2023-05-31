ddeb94138c900ff6f9028c139410f5ddd1369ac9
David Craig <david@dcraig.dev>
2023-05-31 11:03:06 +0100

    Add june earnings.

--- data/projects/hydrowhales/earnings.ts
+++ data/projects/hydrowhales/earnings.ts
@@ -5,6 +5,7 @@ const earnings: EarningsEntry[] = [
   { label: '1 Mar 23', date: '2023-03-01', ethereum: { usdc: 13, wbtc: 10 }, amount: 23.00, notes: '$13 USDC, 0.000424 wBTC' },
   { label: '1 Apr 23', date: '2023-04-01', polygon: { usdc: 0.01, wbtc: 10 }, amount: 23.00, notes: '$0.01 USDC, 0.000346 wBTC' },
   { label: '1 May 23', date: '2023-05-01', polygon: { usdc: 5, wbtc: 5 }, amount: 10, notes: '$5 USDC, 0.000179 wBTC' },
+  { label: '1 Jun 23', date: '2023-06-01', polygon: { usdc: 5, wbtc: 10 }, amount: 15, notes: '$5 USDC, ? wBTC' },
 ]
 
 export default earnings
--- data/projects/project79/burlcore/earnings.ts
+++ data/projects/project79/burlcore/earnings.ts
@@ -2,6 +2,7 @@ import type { EarningsEntry } from "../../../../types/EarningsEntry"
 
 const earnings: EarningsEntry[] = [
   { label: '1 May 23', date: '2023-05-01', ethereum: { usdc: 3.01 }, amount: 3.01 },
+  { label: '2 Jun 23', date: '2023-06-02', ethereum: { usdc: 4.55 }, amount: 4.55 },
 ]
 
 export default earnings
