d63fc4ae70a78e2d0d3e664cb8e27d58b3efadda
David Craig <davidcraig-uk@protonmail.com>
2023-07-31 16:56:20 +0100

    Update earnings.ts
    Add End-July earnings from June

--- data/projects/aceminers/earnings.ts
+++ data/projects/aceminers/earnings.ts
@@ -2,7 +2,8 @@ import type { EarningsEntry } from "../../../types/EarningsEntry";
 
 const earnings: EarningsEntry[] = [
   { label: 'Oct 22', date: '2022-10-20', amount: 15.5, polygon: { weth: 0.012 } }, /// https://polygonscan.com/tx/0xb2124acb1e32aaeb4d755e52d5895cb632299c8fc3e07447ecefbc1375e3b3c5
-  { label: 'Jun 23', date: '2023-06-29', litecoin: { ltc: 0.144 }, kadena: { kda: 1.6 }, nervos: { ckb: 425 } }
+  { label: 'Jun 23', date: '2023-06-29', litecoin: { ltc: 0.144 }, kadena: { kda: 1.6 }, nervos: { ckb: 425 } },
+  { label: 'Jul 23', date: '2023-07-31', litecoin: { ltc: 0.0538 }, kadena: { kda: 1.4 }, nervos: { ckb: 290 } },
 ]
 
 export default earnings
