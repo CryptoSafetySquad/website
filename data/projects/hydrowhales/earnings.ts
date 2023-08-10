76c4ef26e16f41355c0ed9101374ae72d4bedad6
davidcraig <davidcraig1986@gmail.com>
2023-08-10 19:38:26 +0100

    Add jul and aug hwmc earnings

--- data/projects/hydrowhales/earnings.ts
+++ data/projects/hydrowhales/earnings.ts
@@ -6,6 +6,8 @@ const earnings: EarningsEntry[] = [
   { label: '1 Apr 23', date: '2023-04-01', polygon: { usdc: 0.01, wbtc: 10 }, amount: 23.00, notes: '$0.01 USDC, 0.000346 wBTC' },
   { label: '1 May 23', date: '2023-05-01', polygon: { usdc: 5, wbtc: 5 }, amount: 10, notes: '$5 USDC, 0.000179 wBTC' },
   { label: '1 Jun 23', date: '2023-06-01', polygon: { usdc: 5, wbtc: 10 }, amount: 15, notes: '$5 USDC, 0.000373 wBTC' },
+  { label: '1 Jul 23', date: '2023-07-01', ethereum: {} },
+  { label: '1 Aug 23', date: '2023-07-01', polygon: { wbtc: 10 } },
 ]
 
 export default earnings
