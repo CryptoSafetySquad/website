626fa489b8737b8ccc7824f2a5277f978ffda7c4
davidcraig <davidcraig1986@gmail.com>
2023-09-21 19:18:37 +0100

    Update fonts.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,10 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import { Roboto_Mono } from '@next/font/google'
-
-const RobotoMono = Roboto_Mono({
-  weight: ["100", "200", "400", "600"]
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <style jsx global>{`
-        html {
-          font-family: ${RobotoMono.style.fontFamily};
-        }
-      `}</style>
-      <Component className={RobotoMono.className} {...pageProps} />
+      <Component {...pageProps} />
     </>
   )
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Serif&display=optional"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
