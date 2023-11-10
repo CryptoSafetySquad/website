5e013271b13d141a6a0ea3c3d9d37afcf3738ed1
davidcraig <davidcraig-uk@protonmail.com>
2023-11-10 17:32:16 +0000

    Add missing burlcore earnings entries.

--- data/projects/project79/burlcore/earnings.ts
+++ data/projects/project79/burlcore/earnings.ts
@@ -3,6 +3,10 @@ import type { EarningsEntry } from "../../../../types/EarningsEntry"
 const earnings: EarningsEntry[] = [
   { label: '1 May 23', date: '2023-05-01', ethereum: { usdc: 3.01 }, amount: 3.01 },
   { label: '2 Jun 23', date: '2023-06-02', ethereum: { usdc: 4.55 }, amount: 4.55 },
+  { label: '4 Jul 23', date: '2023-07-04', ethereum: { usdc: 4.85 }, amount: 4.85 },
+  { label: '2 Aug 23', date: '2023-08-02', ethereum: { usdc: 3.45 }, amount: 3.45 },
+  { label: '4 Sep 23', date: '2023-09-04', ethereum: { usdc: 3.34 }, amount: 3.34 },
+  { label: '7 Oct 23', date: '2023-10-07', ethereum: { usdc: 2.55 }, amount: 2.55 },
 ]
 
 export default earnings
