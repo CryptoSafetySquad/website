58bf94a3acd56958a9fb40d806e469b5e7be84fe
davidcraig <davidcraig-uk@protonmail.com>
2023-11-10 17:23:17 +0000

    Add Sept and Oct earnings for Ace

--- data/projects/aceminers/earnings.ts
+++ data/projects/aceminers/earnings.ts
@@ -2,8 +2,10 @@ import type { EarningsEntry } from "../../../types/EarningsEntry";
 
 const earnings: EarningsEntry[] = [
   { label: 'Oct 22', date: '2022-10-20', amount: 15.5, polygon: { weth: 0.012 } }, /// https://polygonscan.com/tx/0xb2124acb1e32aaeb4d755e52d5895cb632299c8fc3e07447ecefbc1375e3b3c5
-  { label: 'Jun 23', date: '2023-06-29', litecoin: { ltc: 0.144 }, kadena: { kda: 1.6 }, nervos: { ckb: 425 } },
-  { label: 'Jul 23', date: '2023-07-31', litecoin: { ltc: 0.0538 }, kadena: { kda: 1.4 }, nervos: { ckb: 290 } },
+  { label: 'Jul 23', date: '2023-07-01', litecoin: { ltc: 0.144 }, kadena: { kda: 1.6 }, nervos: { ckb: 425 } },
+  { label: 'Aug 23', date: '2023-08-01', litecoin: { ltc: 0.0538 }, kadena: { kda: 1.4 }, nervos: { ckb: 290 } },
+  { label: 'Sep 23', date: '2023-09-01', litecoin: { ltc: 0.0410 }, kadena: { kda: 1 }, nervos: { ckb: 300 } },
+  { label: 'Oct 23', date: '2023-10-01', litecoin: { ltc: 0.0360 }, kadena: { kda: 1.1 }, nervos: { ckb: 190 } },
 ]
 
 export default earnings
