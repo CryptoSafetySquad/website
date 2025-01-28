47cae7074644b5fa77acc601ab42fd561f286f04
David Craig <davidcraig-uk@protonmail.com>
2025-01-28 22:43:22 +0000

    Add extra decimal in trillion marketcap.

--- functions/marketCap.ts
+++ functions/marketCap.ts
@@ -1,5 +1,6 @@
 export default function marketCap(value: number) {
-  if (value >= 1000000000000) { return `${(value / 1000000000000).toFixed(0)}T` }
+  if (value >= 2000000000000) { return `${(value / 1000000000000).toFixed(0)}T` }
+  if (value >= 1000000000000 && value < 2000000000000) { return `${(value / 1000000000000).toFixed(2)}T` }
   if (value >= 1000000000) { return `${(value / 1000000000).toFixed(0)}B` }
   if (value >= 1000000) { return `${(value / 1000000).toFixed(0)}M` }
   if (value > 1000) { return `${(value / 1000).toFixed(0)}k` }
