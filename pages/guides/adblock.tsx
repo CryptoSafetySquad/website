aed45d4b975ca0db475a9b2c9c4a0287a49add2c
David Craig <davidcraig-uk@protonmail.com>
2025-02-17 11:40:09 +0000

    Update adblock.tsx

--- pages/guides/adblock.tsx
+++ pages/guides/adblock.tsx
@@ -20,7 +20,7 @@ const AdBlock: NextPage = () => {
         <li>under custom click import</li>
         <li>paste in the URL:</li>
         </ol>
-        <p><span className='string'>https://raw.githubusercontent.com/CryptoSafetySquad/adblock-filters/refs/heads/main/fake-news.txt</span></p>
+        <p><span className='string'>https://raw.githubusercontent.com/CryptoSafetySquad/adblock-filters/refs/heads/main/crypto.txt</span></p>
         <p className='mb-8'>Click apply changes and the lists filters should now be applied in the browser, uBo will now automatically keep this list up to date regularly.</p>
         <p>Note: uBlock does also include a lot of other filter lists out of the box, if you dont wan&apos;t to use any of these you can simply untick them, I personally keep them all active.</p>
         <p>I hope you found this guide useful and that it helps keep you that little bit more secure in your crypto journey.</p>
